3c317914deddba33ac02152bab40cb1403ef282a
Fabio Cunha <fabiocunhaosp@gmail.com>
2026-09-23 15:11:20 -0300

    feat: adiciona controle de permissão por módulo

--- apps/accounts/decorators.py
+++ apps/accounts/decorators.py
@@ -14,3 +14,16 @@ def system_creator_required(view_func):
             raise PermissionDenied
         return view_func(request, *args, **kwargs)
     return wrapped
+
+
+def module_permission(permission):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapped(request, *args, **kwargs):
+            if not request.user.is_authenticated:
+                return redirect(f"{reverse('login')}?next={request.path}")
+            if request.user.is_system_creator or request.user.has_perm(permission):
+                return view_func(request, *args, **kwargs)
+            raise PermissionDenied
+        return wrapped
+    return decorator
